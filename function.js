663abf043fdc6d0cff68575c1b5764d8116152c8
idchavez <ivan971325@gmail.com>
2026-04-13 11:49:16 -0500

    funcion guardar

--- function.js
+++ function.js
@@ -4,28 +4,81 @@ let imagenPokemon = document.getElementById("imagenPokemon");
 const pokemonActual = null;
 const url = " https://pokeapi.co/api/v2/pokemon";
 
-
 /* Funcion Buscar pokemón*/
 function searchPokemon() {
+  const nombre = document.getElementById("nombrePokemon").value.trim();
+
+     
   fetch(url + nombre)
     .then(function (response) {
-        if (response) {
-             return response.json();
-        }
-        else {
-            alert("Pokémon no encontrado")
-        }
+      if (response.ok) {
+        return response.json();
+      } else {
+        alert("Pokémon no encontrado");
+        return;
+      }
     })
     .then(function (data) {
       // Datos del Pokémon aquí
-      nombrePokemon.textContent =  data.name;
-      imagenPokemon.textContent =  data.sprites.front_default;
-
-      console.log(data);
+      nombrePokemon.textContent = data.name;
+      imagenPokemon.src = data.sprites.front_default;
+      pokemonGlobal = data;
+      console.log(pokemonGlobal);
     })
     .catch(function (error) {
       alert("¡Error! Pokémon no encontrado");
     });
+    
 }
 
-searchPokemon();
+buscarPokemon.addEventListener("click", searchPokemon)
+
+
+
+function saveFavorite() {
+  if (!pokemonActual) {
+    alert("Busca un Pokemon");
+    return;
+  }
+
+  const favoritos = obtenerFavorites();
+
+  const existe = favoritos.some(p => p.name === pokemonActual.name);
+
+  if (existe) {
+    alert("Ya esta en favoritos");
+    return;
+  }
+
+  favoritos.push(pokemonActual);
+  setearFavoritos(favoritos);
+
+  mostrarFavoritos();
+}
+
+
+function obtenerFavorites() {
+  return JSON.parse(localStorage.getItem("favoritos")) || [];
+}
+
+function setearFavoritos(favoritos) {
+  localStorage.setItem("favoritos", JSON.stringify(favoritos));
+}
+
+function mostrarFavoritos() {
+  const contenedor = document.getElementById("favoritos");
+  container.innerHTML = "";
+
+  const favoritos = obtenerFavorites();
+
+  favoritos.forEach(pokemon => {
+    const div = document.createElement("div");
+
+    div.innerHTML = `
+      <p>${pokemon.name}</p>
+      <img src="${pokemon.image}" width="80">
+    `;
+
+    contenedor.appendChild(div);
+  });
+}
